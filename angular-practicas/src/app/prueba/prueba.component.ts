f3affb32f46e1ca0165a220a896e71ae6e372f44
Jorgextz <jorgeleo.marcano@gmail.com>
2024-06-10 15:42:16 -0300

    Directiva for terminada

--- angular-practicas/src/app/prueba/prueba.component.ts
+++ angular-practicas/src/app/prueba/prueba.component.ts
@@ -18,6 +18,13 @@ usuario:Persona = {
   edad: 27
 }
 
+personas:Persona[] = [
+  {nombre: 'Jorge Marcano', edad:31},
+  {nombre: 'Martin Bragatto', edad:57},
+  {nombre: 'Mauro Bragatto', edad:25},
+  {nombre: 'Sabrina Bragatto', edad:58}
+]
+
 numero:number = 0;
 text_color:string ='';
 button_change:boolean = false;
@@ -25,6 +32,8 @@ button_change:boolean = false;
 img:string = 'https://images.pexels.com/photos/23947602/pexels-photo-23947602/free-photo-of-amor-gente-mujer-picnic.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
 
 ActiveAlert:boolean= false;
+
+dia:number = 1;
 
 
 // Metodos
